031fe4f72ae786926342dc5926d98e1acb2d08d1
David Estrada <david_edgardo@unitec.edu>
2026-09-10 07:14:46 -0600

    Ventana

---
+++ Prueba/src/VentanaDescargas.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class VentanaDescargas extends JFrame {
+
+    public VentanaDescargas() {
+
+        setTitle("Descarga Múltiple");
+        setSize(450, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
